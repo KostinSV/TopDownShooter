47c42271dc7e39031db4b285b144ae6895ebd329
Your Name <youremail@domain.com>
2021-01-26 14:18:03 +0300

    ok

--- main.lua
+++ main.lua
@@ -36,7 +36,7 @@ function love.draw()
     love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
 
     for i, z in ipairs (zombies) do
-        love.graphics.draw(sprites.zombie, z.x, z.y)
+        love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z))
     end
 end    
 
@@ -51,7 +51,7 @@ function playerMouseAngle()
 end
 
 function zombiePlayerAngle(enemy)
-    return math.atan2(player.y - enemy.y, player.x - enemy.x) + math.pi
+    return math.atan2(player.y - enemy.y, player.x - enemy.x)
 end
 
 function spawnZombie()
