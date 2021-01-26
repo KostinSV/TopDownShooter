302471ee5d5b22f850404a1753cc83b0f305b7b8
Your Name <youremail@domain.com>
2021-01-26 18:19:25 +0300

    with bullet from center

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function love.load()
-    sprites ={}
+    sprites = {}
     sprites.background = love.graphics.newImage('sprites/background.png')
     sprites.bullet = love.graphics.newImage('sprites/bullet.png')
     sprites.player = love.graphics.newImage('sprites/player.png')
@@ -11,6 +11,7 @@ function love.load()
     player.speed = 180
 
     zombies = {}
+    bullets = {}
 
 end
 
@@ -32,12 +33,18 @@ function love.update(dt)
         z.x = z.x + math.cos(zombiePlayerAngle(z)) * z.speed * dt
         z.y = z.y + math.sin(zombiePlayerAngle(z)) * z.speed * dt
         
-        if distanceBetween(z.x, z.y, player.x, player.y) <l 30 then
+        if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
             for i,z in ipairs(zombies) do
                 zombies[i] = nil
             end 
         end
     end
+
+    for i,b in ipairs(bullets) do
+        b.x = b.x + (math.cos(b.direction) * b.speed * dt)
+        b.y = b.y + (math.sin(b.direction) * b.speed * dt)
+    end
+
 end
 
 function love.draw()
@@ -48,11 +55,21 @@ function love.draw()
     for i, z in ipairs (zombies) do
         love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
     end
+
+    for i, b in ipairs (bullets) do
+        love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.5, nil, sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2 )
+    end
 end    
 
 function love.keypressed(key)
     if key == 'space' then
         spawnZombie()
+    end
+end
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        spawnBullet()
     end
 end
 
@@ -69,7 +86,16 @@ function spawnZombie()
     zombie.x = math.random(0, love.graphics.getWidth())
     zombie.y = math.random(0, love.graphics.getHeight())
     zombie.speed = 140
-    table.insert(zombies, zombie)
+    table.insert (zombies, zombie)
+end
+
+function spawnBullet()
+    local bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = 500
+    bullet.direction = playerMouseAngle()
+    table.insert (bullets, bullet)
 end
 
 function distanceBetween(x1, y1, x2, y2)
